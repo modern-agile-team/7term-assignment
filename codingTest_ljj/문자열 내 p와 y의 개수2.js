fb6e913393333b6d0310ffdb3f5daf3ed3e5bf58
cyoure <wwj9559@gmail.com>
2023-12-04 18:33:17 +0900

    문자열 내 p와 y의 개수 다른 풀이

--- codingTest_ljj/문자열 내 p와 y의 개수2.js
+++ codingTest_ljj/문자열 내 p와 y의 개수2.js
@@ -1,18 +1,13 @@
-function solution(s){
-    let pNum = 0;
-    let yNum = 0;
-    
-    for (let i =0; i<s.length; i++)
-        {
-            if(s[i] === "p" || s[i] === "P" )
-                {
-                    pNum++;
-                   
-                }
-           else if(s[i] === "y" || s[i] === "Y")
-                {
-                    yNum++;
-                }
-        }
-     return pNum === yNum ? true : false;
- }
+function solution(s) {
+  let pNum = 0;
+  let yNum = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "p" || s[i] === "P") {
+      pNum++;
+    } else if (s[i] === "y" || s[i] === "Y") {
+      yNum++;
+    }
+  }
+  return pNum === yNum ? true : false;
+}
