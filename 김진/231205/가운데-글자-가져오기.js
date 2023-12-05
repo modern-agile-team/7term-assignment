b4835e57cfbbe9dbec62742fe2f4190212bbe39f
chamjin <playingme@naver.com>
2023-12-05 16:29:24 +0900

    :smailing_face_with_open_hands: 2023-12-05 김진

---
+++ 김진/231205/가운데-글자-가져오기.js
@@ -0,0 +1,13 @@
+1;
+2;
+3;
+4;
+5;
+6;
+7;
+function solution(s) {
+  if (s.length % 2 !== 0) {
+    return s[Math.floor(s.length / 2)];
+  }
+  return `${s[s.length / 2 - 1]}${s[s.length / 2]}`;
+}
