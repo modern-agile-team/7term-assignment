9d99758aee95a5faecaf5c1ba37ec6a0b5d2fd5e
cyoure <wwj9559@gmail.com>
2023-12-21 17:29:53 +0900

    모의고사

--- codingTest_김준우/체육복.js
+++ codingTest_김준우/체육복.js
@@ -1,4 +1,10 @@
 function solution(n, lost, reserve) {
+  lost = lost.sort((a, b) => {
+    return a - b;
+  });
+  reserve = reserve.sort((a, b) => {
+    return a - b;
+  });
   let same = lost.filter((data) => reserve.includes(data));
   let total = n - lost.length;
   if (same.length === 0) {
@@ -6,8 +12,8 @@ function solution(n, lost, reserve) {
       for (let j = 0; j < reserve.length; j++) {
         if (lost[i] - reserve[j] === 1 || lost[i] - reserve[j] === -1) {
           total += 1;
-          lost.splice(i, 1);
-          reserve.splice(j, 1);
+          lost.splice(i, 1, -1);
+          reserve.splice(j, 1, -1);
         }
       }
     }
@@ -16,36 +22,29 @@ function solution(n, lost, reserve) {
     for (let i = 0; i < same.length; i++) {
       for (let j = 0; j < lost.length; j++) {
         if (same[i] === lost[j]) {
-          lost.splice(j, 1);
-          j-=1;
+          lost.splice(j, 1, -1);
         }
       }
     }
-    console.log(lost);
     for (let i = 0; i < same.length; i++) {
       for (let j = 0; j < reserve.length; j++) {
         if (same[i] === reserve[j]) {
-          reserve.splice(j, 1);
-          j-=1;
+          reserve.splice(j, 1, -1);
         }
       }
     }
-    console.log(reserve);
     for (let i = 0; i < lost.length; i++) {
       for (let j = 0; j < reserve.length; j++) {
         if (lost[i] - reserve[j] === 1 || lost[i] - reserve[j] === -1) {
           total += 1;
-          lost.splice(i, 1);
-          reserve.splice(j, 1);
-          i -= 1;
-          j -= 1;
+          lost.splice(i, 1, -1);
+          reserve.splice(j, 1, -1);
         }
       }
     }
   }
   return total;
 }
-console.log(solution(9,[1,3,5,6,8],[1,2,4,6,7]));
 // lost / reserve 요소 같은 게 있는지 -> 여벌 체육복 해당 같은 요소 친구가 입어야함
 // 겹치는 경우가 있는 경우 없는 경우
 // 그외 reserve 요소 앞 뒤 숫자에 lost가 있는지
