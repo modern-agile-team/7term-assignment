eabc48dc7cfcb29b71718578369d72cebe592478
jinwoo0207 <a91287840@gmail.com>
2024-01-05 13:10:41 +0900

    키패드 누르기

---
+++ codingtest_ajw/22일~/계산기.js
@@ -0,0 +1,17 @@
+let number = document.getElementById('number');
+
+// console.log(number);
+number.replace(/ /g,"");
+
+function multiply(x , y){
+    return (x * y);
+}
+function divide(x, y){
+    return (x / y);
+}
+function add(x, y){
+    return (x + y);
+}
+function remove(x, y){
+    return (x - y);
+}
